aaa7c02906a08d95352ce0672fba5ea7fadd08f3
Valentin <valentin.lucet@gmail.com>
2021-08-08 11:50:33 -0400

    updates

--- scripts/1_base_script.R
+++ scripts/1_base_script.R
@@ -7,11 +7,15 @@ library(dplyr)
 
 # 1. Download Mauna Loa data ----------------------------------------------
 
-weekly_data_url <- "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_weekly_mlo.txt"
-raw_data <- read.table(weekly_data_url, skip = 49)
-names(raw_data) <- c("year", "month", "day", "year_decimal", "co2_ppm", "nb_days", 
-                     "1_year_ago", "10_years_ago", "increase_since_1980")
-clean_data <- raw_data %>% 
+weekly_data_url <- 
+  "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_weekly_mlo.txt"
+co2_data <- read.table(weekly_data_url, skip = 49)
+
+names(co2_data) <- c("year", "month", "day", "year_decimal", 
+                     "co2_ppm", "nb_days", "1_year_ago", 
+                     "10_years_ago", "increase_since_1980")
+
+co2_data_clean <- co2_data %>% 
   mutate(date = ymd(paste(year, month, day, sep = "-"))) %>% 
   select(-year, -month, -day) %>% 
   relocate(date)
@@ -20,9 +24,10 @@ clean_data <- raw_data %>%
 
 birthday <- ymd("1996-10-29")
 
-ppm_from_date <- clean_data %>% 
+ppm_from_date <- co2_data_clean %>% 
   mutate(diff = abs(date - birthday)) %>% 
   arrange(diff) %>% 
-  slice(1) %>% pull(co2_ppm)
+  slice(1) %>% 
+  pull(co2_ppm)
 
 print(ppm_from_date)
--- scripts/2_functions.R
+++ scripts/2_functions.R
@@ -5,11 +5,13 @@
 
 get_weekly_data <- function(clean = TRUE){
   
-  url <- "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_weekly_mlo.txt"
+  weekly_data_url <- 
+    "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_weekly_mlo.txt"
   
-  co2_data <- read.table(url, skip = 49)
-  names(co2_data) <- c("year", "month", "day", "year_decimal", "co2_ppm", "nb_days", 
-                       "1_year_ago", "10_years_ago", "increase_since_1980")
+  co2_data <- read.table(weekly_data_url, skip = 49)
+  names(co2_data) <- c("year", "month", "day", "year_decimal", "co2_ppm", 
+                       "nb_days", "1_year_ago", "10_years_ago", 
+                       "increase_since_1980")
   
   if (clean) {
     co2_data <- co2_data %>% 
@@ -27,9 +29,9 @@ ppm_from_date <- function(date){
   
   date_formatted <- lubridate::ymd("1996-10-29")
   
-  clean_data <- get_weekly_data()
+  co2_data_clean <- get_weekly_data(clean = TRUE)
   
-  ppm <- clean_data %>% 
+  ppm <- co2_data_clean %>% 
     dplyr::mutate(diff = abs(date - date_formatted)) %>% 
     dplyr::arrange(diff) %>% 
     dplyr::slice(1) %>% 
