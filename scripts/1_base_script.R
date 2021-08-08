3c4beda899c28fad9e8adf28e9ee0b682ba41930
Valentin <valentin.lucet@gmail.com>
2021-08-08 12:36:07 -0400

    replace pipes

--- scripts/1_base_script.R
+++ scripts/1_base_script.R
@@ -15,19 +15,19 @@ names(co2_data) <- c("year", "month", "day", "year_decimal",
                      "co2_ppm", "nb_days", "1_year_ago", 
                      "10_years_ago", "increase_since_1980")
 
-co2_data_clean <- co2_data %>% 
-  mutate(date = ymd(paste(year, month, day, sep = "-"))) %>% 
-  select(-year, -month, -day) %>% 
+co2_data_clean <- co2_data |> 
+  mutate(date = ymd(paste(year, month, day, sep = "-"))) |> 
+  select(-year, -month, -day) |> 
   relocate(date)
 
 # 2. Find concentration closest to a date ---------------------------------
 
 birthday <- ymd("1996-10-29")
 
-ppm_from_date <- co2_data_clean %>% 
-  mutate(diff = abs(date - birthday)) %>% 
-  arrange(diff) %>% 
-  slice(1) %>% 
+ppm_from_date <- co2_data_clean |> 
+  mutate(diff = abs(date - birthday)) |> 
+  arrange(diff) |> 
+  slice(1) |> 
   pull(co2_ppm)
 
 print(ppm_from_date)
