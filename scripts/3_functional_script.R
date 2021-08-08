79df46f684701ffb2079a072c9f1f06e43133891
Valentin <valentin.lucet@gmail.com>
2021-08-08 10:36:24 -0400

    reset scripts

--- scripts/3_functional_script.R
+++ scripts/3_functional_script.R
@@ -1,14 +1,2 @@
-# My eBird map
-# functionnal script
-
-# Load packages
-library(dplyr)     # To manipulate data
-library(janitor)   # To clean data
-library(lubridate) # To clean date
-library(sf)        # To handle vector data
-library(leaflet)   # To make nice maps
-library(htmltools) # To make nice map marker labels
-
-source("scripts/2_functions.R")
-
-my_ebird_map("data/raw_ebird_data.csv")
+# Package {carbon}
+# Functional script
