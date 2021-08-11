e993e447c713494a72db7f9a19c17f213e75225c
Valentin <valentin.lucet@gmail.com>
2021-08-11 10:17:19 -0400

    updates

--- scripts/2_functions.R
+++ scripts/2_functions.R
@@ -9,15 +9,17 @@ get_weekly_data <- function(clean = TRUE) {
     "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_weekly_mlo.txt"
   
   co2_data <- read.table(weekly_data_url, skip = 49)
-  names(co2_data) <- c("year", "month", "day", "year_decimal", 
-                       "co2_ppm", "nb_days", "1_year_ago", 
-                       "10_years_ago", "increase_since_1980")
   
   if (clean) {
-    co2_data <- co2_data |> 
+    
+    names(co2_data) <- c("year", "month", "day", "year_decimal", 
+                         "co2_ppm", "nb_days", "1_year_ago", 
+                         "10_years_ago", "increase_since_1980")
+    
+    co2_data <- co2_data %>% 
       dplyr::mutate(date = lubridate::ymd(paste(year, month, day, 
-                                                sep = "-"))) |> 
-      dplyr::select(-year, -month, -day) |> 
+                                                sep = "-"))) %>% 
+      dplyr::select(-year, -month, -day) %>% 
       dplyr::relocate(date)
   } 
   
@@ -32,10 +34,10 @@ ppm_from_date <- function(date) {
   
   co2_data_clean <- get_weekly_data(clean = TRUE)
   
-  ppm <- co2_data_clean |> 
-    dplyr::mutate(diff = abs(date - date_formatted)) |> 
-    dplyr::arrange(diff) |> 
-    dplyr::slice(1) |> 
+  ppm <- co2_data_clean %>% 
+    dplyr::mutate(diff = abs(date - date_formatted)) %>% 
+    dplyr::arrange(diff) %>% 
+    dplyr::slice(1) %>% 
     dplyr::pull(co2_ppm)
   
   return(ppm)
